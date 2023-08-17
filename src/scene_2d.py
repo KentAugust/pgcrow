b3fe93772367945f9e37f14669802991fbb7de3f
KentAugust <kentaugustone@gmail.com>
2023-08-17 15:16:48 -0600

    Fix window param

--- src/scene_2d.py
+++ src/scene_2d.py
@@ -1,5 +1,9 @@
 """## Scene 2D"""
 
+import pygame
+
+from .window import Window
+
 
 class Scene2D:
     """Class representing a single game scene"""
@@ -8,8 +12,16 @@ class Scene2D:
         self.game = game
         self.game.event_handler.loop = self.handle_events
 
-    def handle_events(self):
+    def handle_events(self, window: Window):
         """Custom event handling method"""
+
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    window.quit()
+                case pygame.KEYDOWN:
+                    if event.key == pygame.K_F11:
+                        window.toggle_fullscreen()
 
     def update(self, dt: float):
         """For updating stuff"""
