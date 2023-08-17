52a16ecf7b4bd55c9729249c1bd3b272a3951ee5
KentAugust <kentaugustone@gmail.com>
2023-08-17 16:23:07 -0600

    Add three types of timers

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,6 @@
 from . import config
 from . import window
 from . import event_handler
+from . import timers
 from . import game
 from . import scene_2d
---
+++ src/timers.py
@@ -0,0 +1,78 @@
+"""## Timers"""
+
+import time
+
+
+class Timer:
+    """Basic timer"""
+
+    def __init__(self) -> None:
+        self.__time = time.time()
+
+    @property
+    def seconds(self) -> float:
+        """Get time in seconds"""
+
+        self.__time = time.time()
+        return self.__time % 60
+
+    @property
+    def minutes(self) -> float:
+        """Get time in minutes"""
+
+        self.__time = time.time()
+        return (self.__time % 3600) // 60
+
+    @property
+    def hours(self) -> float:
+        """Get time in hours"""
+
+        self.__time = time.time()
+        return self.__time // 3600
+
+
+class Deltatimer:
+    """Timer for deltatime"""
+
+    def __init__(self) -> None:
+        self.__dt = 0
+        self.__prev_time = time.time()
+        super().__init__()
+
+    def get_dt(self) -> float:
+        """Get deltatime in seconds"""
+
+        self.__dt = time.time() - self.__prev_time
+        self.__prev_time = time.time()
+        return self.__dt
+
+    @property
+    def dt(self) -> float:
+        """Returns deltatime"""
+        return self.__dt
+
+
+class Cronometer:
+    """Cronometer for counting"""
+
+    def __init__(self) -> None:
+        self.__current_time = 0
+        self.__start_time = 0
+
+    def update(self, dt: float) -> float:
+        """Update the currente time"""
+
+        self.__start_time += dt
+        self.__current_time = self.__start_time * 1000
+        return self.__current_time
+
+    def reset(self):
+        """Set current time to 0"""
+
+        self.__start_time = 0
+        self.__current_time = 0
+
+    @property
+    def current_time(self) -> float:
+        """Get currente time"""
+        return self.__current_time
