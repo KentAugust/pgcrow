25a45130443c03e5fe4069b1861db4104e1e3166
KentAugust <kentaugustone@gmail.com>
2023-08-21 16:49:30 -0600

    Fix setup to include examples

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author="KentAugust",
     author_email="kentaugustone@gmail.com",
     url="https://github.com/KentAugust/pycrow",
-    package_dir={"pgcrow": "src"},
+    package_dir={"pgcrow": "src", "pgcrow.examples": "examples"},
     install_requires=['pygame-ce>=2.3.1'],
 )
