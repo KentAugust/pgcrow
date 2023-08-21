ddc97c4fc612a1f405970e4f86323eded1cf98f2
KentAugust <kentaugustone@gmail.com>
2023-08-21 16:51:09 -0600

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pgcrow",
-    version="0.1.0",
+    version="0.2.0",
     description="Pygame framework for making simple games",
     author="KentAugust",
     author_email="kentaugustone@gmail.com",
