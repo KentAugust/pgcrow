490b0e278c52fb6b91098db11af06223f683496b
KentAugust <kentaugustone@gmail.com>
2023-08-23 12:07:44 -0600

    Remove fullcreen action

--- src/event_handler.py
+++ src/event_handler.py
@@ -10,11 +10,11 @@ class EventHandler:
     """Class for hangling all the events"""
 
     def loop(self, window: Window, keyboard: Keyboard, mouse: Mouse):
+        """Event loop"""
+
         for event in pygame.event.get():
             keyboard.handle_event(event)
             mouse.handle_event(event)
             match event.type:
                 case pygame.QUIT:
                     window.quit()
-        if keyboard.pressed_this_frame(pygame.K_F11):
-            window.toggle_fullscreen()
