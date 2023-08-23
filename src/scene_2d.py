346f8d62f217bb63590963fe1467e6b001c237d7
KentAugust <kentaugustone@gmail.com>
2023-08-23 11:37:49 -0600

    Add Game import

--- src/scene_2d.py
+++ src/scene_2d.py
@@ -2,26 +2,14 @@
 
 import pygame
 
-from .inputs import Keyboard, Mouse
-from .window import Window
+from .game import Game
 
 
 class Scene2D:
     """Class representing a single game scene"""
 
-    def __init__(self, game) -> None:
+    def __init__(self, game: Game) -> None:
         self.game = game
-        self.game.event_handler.loop = self.handle_events
-
-    def handle_events(self, window: Window, keyboard: Keyboard, mouse: Mouse):
-        """Custom event handling method"""
-
-        for event in pygame.event.get():
-            keyboard.handle_event(event)
-            mouse.handle_event(event)
-            match event.type:
-                case pygame.QUIT:
-                    window.quit()
 
     def update(self, dt: float):
         """For updating stuff"""
