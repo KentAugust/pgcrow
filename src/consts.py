e3e49af31ff99852a0b257890470719734d85bff
KentAugust <kentaugustone@gmail.com>
2023-09-02 10:42:43 -0600

    Add consts file

---
+++ src/consts.py
@@ -0,0 +1,20 @@
+"""Constants"""
+
+from enum import Enum, StrEnum
+
+
+class ScaleFuntions(StrEnum):
+    """Enum for scale funtion on WindowConfig"""
+
+    SMOOTH = "smooth"
+    NEAREST = "nearest"
+
+
+class Math(Enum):
+    """Math related enum"""
+
+    PI = 3.141592653589793
+    TAU = 6.283185307179586
+    PHI = 1.618033988749895
+    E = 2.718281828459045
+    G = 9.80665
