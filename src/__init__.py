f3fcc41782f4fda434479c8a786e8ca034eb17ca
KentAugust <kentaugustone@gmail.com>
2023-09-10 08:16:37 -0600

    Rename sprite file

--- src/__init__.py
+++ src/__init__.py
@@ -6,5 +6,5 @@ from .event_handler import EventHandler
 from .game import Game
 from .scene_2d import Scene2D
 from .scene_manager import SceneManager
-from .sprite_sheet import SpriteSheet
+from .sprites import SpriteSheet
 from .window import WindowDisplay, WindowScreen
