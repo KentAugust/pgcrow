67aa499b73976d00aee17241a29ae9b8d36079af
KentAugust <kentaugustone@gmail.com>
2023-09-16 15:38:56 -0600

    Add stringify and unstringify

--- src/maths.py
+++ src/maths.py
@@ -8,13 +8,43 @@ import pygame
 class Vec2(pygame.Vector2):
     """Wrapper for pygame.Vector2"""
 
-    # It desn't add nothing new for now
+    def stringify(self, sep: str = ",") -> str:
+        """Returns a vector representation with the given separator
+        ie. with "," as separator:
+        Vec2(4.0, 2.23) -> "4.0,2.23"
+        """
+        return f"{self.x}{sep}{self.y}"
+
+    @staticmethod
+    def unstringify(string: str, sep: str = ",") -> "Vec2":
+        """Returns a new Vect2 from a string
+        ie. with "," as separator:
+        "4.0,2.23" -> Vec2(4.0, 2.23)
+        """
+        vec_elements = string.split(sep)
+        x, y = vec_elements[0], vec_elements[1]
+        return Vec2(float(x), float(y))
 
 
 class Vec3(pygame.Vector3):
     """Wrapper for pygame.Vector3"""
 
-    # It desn't add nothing new for now
+    def stringify(self, sep: str = ",") -> str:
+        """Returns a vector representation with the given separator
+        ie. with "," as separator:
+        Vec3(4.0, 2.23, 0.52) -> "4.0,2.23,0.52"
+        """
+        return f"{self.x}{sep}{self.y}{sep}{self.z}"
+
+    @staticmethod
+    def unstringify(string: str, sep: str = ",") -> "Vec3":
+        """Returns a new Vect3 from a string
+        ie. with "," as separator:
+        "4.0,2.23,0.52" -> Vec3(4.0, 2.23, 0.52)
+        """
+        vec_elements = string.split(sep)
+        x, y, z = vec_elements[0], vec_elements[1], vec_elements[2]
+        return Vec3(float(x), float(y), float(z))
 
 
 def deg_to_rad(angle: float) -> float:
