75b821fbaec8b2227b1a918b5f79dc10df1620be
KentAugust <kentaugustone@gmail.com>
2023-09-16 08:52:53 -0600

    Add pause method

--- src/animation.py
+++ src/animation.py
@@ -7,27 +7,31 @@ from .consts import FrameData
 from .timers import Chronometer
 
 
-class Animation:
+class Animation:  # pylint: disable=R0902
     """Class for hangling animations"""
 
-    def __init__(
-        self, animation_data: list[FrameData], loop: bool = False
-    ):
+    def __init__(self, animation_data: list[FrameData], loop: bool = False):
         self._animation_data = animation_data
         self.loop = loop
         self.ended = False
+        self.paused = False
         self.frame = 0
         self._flip = [False, False]
         self._current_frame = 0
         self._chronometer = Chronometer()
 
-    def play(self, dt: float, flip_x: bool = False, flip_y: bool = False) -> pygame.Surface:
+    def play(
+        self, dt: float, flip_x: bool = False, flip_y: bool = False
+    ) -> pygame.Surface:
         """Updates the current frame"""
-        if not self.ended:
+        if not self.ended and not self.paused:
             self._chronometer.update(dt)
 
         # update animation frame
-        if self._chronometer.current_time >= self._animation_data[self._current_frame][1]:
+        if (
+            self._chronometer.current_time
+            >= self._animation_data[self._current_frame][1]
+        ):
             self.frame += 1
             if not self.loop and self.frame >= len(self._animation_data):
                 self.frame = len(self._animation_data) - 1
@@ -36,6 +40,11 @@ class Animation:
             self._chronometer.reset()
 
         self._flip = [flip_x, flip_y]
+
+    def pause(self) -> bool:
+        """Pause the animation"""
+        self.paused = not self.paused
+        return self.paused
 
     def stop(self):
         """Restart the animation"""
@@ -51,8 +60,10 @@ class Animation:
     @property
     def image(self) -> pygame.Surface:
         """Get the current frame surface"""
-        return flip(self._animation_data[self._current_frame][0], self._flip[0], self._flip[1])
-    
+        return flip(
+            self._animation_data[self._current_frame][0], self._flip[0], self._flip[1]
+        )
+
     @property
     def animation_data(self) -> list[FrameData]:
         """Get all frames data"""
