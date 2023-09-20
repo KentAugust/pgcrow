8a5d0fead8cf1dd177a269d3b890dd46d1d930a0
KentAugust <kentaugustone@gmail.com>
2023-09-20 08:55:27 -0600

    Change variable names & total_frames parameter

--- src/animation.py
+++ src/animation.py
@@ -10,64 +10,61 @@ from .timers import Chronometer
 class Animation:  # pylint: disable=R0902
     """Base class for hangling animations"""
 
-    def __init__(self, lenght: float, total_frames: int, loop: bool = False):
+    def __init__(self, lenght: float, fps: int = 24, loop: bool = False):
         self.loop = loop
-        self.lenght = lenght
-        self.ended = False
-        self.paused = False
+        self.lenght = max(1, lenght)
+        self.has_ended = False
+        self.is_paused = False
         self._current_frame = 0
-        self._total_frames = total_frames
+        self._fps = max(1, fps)
+        self._total_frames = round(self._fps * self.lenght)
         self._frame_lenght = self.lenght / self._total_frames
         self._frame = 0
-        self._chronometer = Chronometer()
+        self._timer = Chronometer()
 
     def play(self, dt: float):
         """Updates the current frame"""
-        if not self.ended and not self.paused:
-            self._chronometer.update(dt)
+        if not self.has_ended and not self.is_paused:
+            self._timer.update(dt)
 
         # update animation frame
-        if (
-            self._chronometer.current_time
-            >= self._frame_lenght
-        ):
+        if self._timer.current_time >= self._frame_lenght:
             self._current_frame += 1
             if not self.loop and self._current_frame >= self._total_frames:
                 self._current_frame = self._total_frames - 1
-                self.ended = True
+                self.has_ended = True
             self._frame = self._current_frame % self._total_frames
-            self._chronometer.reset()
+            self._timer.reset()
 
     def pause(self) -> bool:
         """Pause the animation"""
-        self.paused = not self.paused
-        return self.paused
+        self.is_paused = not self.is_paused
+        return self.is_paused
 
     def stop(self):
         """Restart the animation"""
-        self._chronometer.reset()
+        self._timer.reset()
         self._current_frame = 0
         self._frame = 0
-        self.ended = False
+        self.has_ended = False
 
     def copy(self) -> "Animation":
         """Return a copy of Animation"""
-        return Animation(self._animation_data, self.loop)
+        return Animation(self.lenght, self._fps, self.loop)
 
 
 class SpriteAnimation(Animation):  # pylint: disable=R0902
     """Class for hangling sprite animations"""
 
     def __init__(self, animation_data: list[FrameData], loop: bool = False):
-        lenght = sum([frame_data[1] for frame_data in animation_data])
-        super().__init__(lenght, len(animation_data), loop)
+        lenght = sum(frame_data[1] for frame_data in animation_data)
+        super().__init__(lenght, len(animation_data)*lenght, loop)
         self._animation_data = animation_data
+        self._total_frames = len(animation_data)
         self._frame_lenght = self._animation_data[0][1]
         self._flip = [False, False]
 
-    def play(
-        self, dt: float, flip_x: bool = False, flip_y: bool = False
-    ):
+    def play(self, dt: float, flip_x: bool = False, flip_y: bool = False):
         """Updates the current frame"""
         super().play(dt)
         self._frame_lenght = self._animation_data[self._frame][1]
@@ -75,15 +72,15 @@ class SpriteAnimation(Animation):  # pylint: disable=R0902
 
     def pause(self) -> bool:
         """Pause the animation"""
-        self.paused = not self.paused
-        return self.paused
+        self.is_paused = not self.is_paused
+        return self.is_paused
 
     def stop(self):
         """Restart the animation"""
-        self._chronometer.reset()
+        self._timer.reset()
         self._current_frame = 0
         self._frame = 0
-        self.ended = False
+        self.has_ended = False
 
     def copy(self) -> "Animation":
         """Return a copy of Animation"""
@@ -92,9 +89,7 @@ class SpriteAnimation(Animation):  # pylint: disable=R0902
     @property
     def image(self) -> pygame.Surface:
         """Get the current frame surface"""
-        return flip(
-            self._animation_data[self._frame][0], self._flip[0], self._flip[1]
-        )
+        return flip(self._animation_data[self._frame][0], self._flip[0], self._flip[1])
 
     @property
     def animation_data(self) -> list[FrameData]:
