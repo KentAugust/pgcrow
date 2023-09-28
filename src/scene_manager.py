2f28aa7fbfbe3bb6a15f0d0158f2ba0cfee7479e
KentAugust <kentaugustone@gmail.com>
2023-09-28 12:35:10 -0600

    Add start_initial_scene method

--- src/scene_manager.py
+++ src/scene_manager.py
@@ -1,12 +1,14 @@
 """Scene Manager"""
 
+from typing import Self
+
 import pygame
 
 from .config import CallableScene, Game
 from .scene_2d import Scene2D
 
 
-class SceneManager:
+class SceneManager:  # pylint: disable=R0902
     """Class for hanglind scenes"""
 
     def __init__(
@@ -15,45 +17,50 @@ class SceneManager:
         initial_name: str = "main",
         initial: CallableScene | None = None,
     ):
-        self.game = game
         self._scenes = {}
+        self._actual_scene = None
+        self._actual_scene_name = None
+        self._previus_scene_name = None
+        self._next_scene_name = None
+        self._run_exit = False
+        self._run_enter = False
+        self._initial_name = initial_name
+        self.game = game
 
         if initial is None:
             initial = CallableScene(Scene2D, {"game": game})
 
-        self._actual_scene = None
-        self._actual_scene_name = None
-        self._run_exit = False
-        self._run_enter = False
         self.add_scene(initial_name, initial)
-        self.change_scene(initial_name)
+
+    def start_initial_scene(self) -> Self:
+        """Instantiate the initial scene"""
+        if not pygame.get_init():
+            raise pygame.error
+        self.change_scene(self._initial_name)
+        return self
 
     def update(self, dt: float):
         """Update current scene"""
-
         self._actual_scene.update(dt)
-        self._update_transtion(dt)
+        self.__update_transition(dt)
 
     def render(self, display: pygame.Surface, offset: tuple[float, float] = (0, 0)):
         """Render current scene"""
-
         self._actual_scene.render(display)
-        self._render_transtion(display)
-        self._actual_scene.render_screen(self.game.window.get_screen(offset))
+        self.__render_transition(display)
+        self._actual_scene.render_screen(self.game.window.screen)
 
-    def _update_transtion(self, dt: float):
+    def __update_transition(self, dt: float):
         """Update the current scene transition"""
-
         if self._run_exit:
             if self._actual_scene.on_exit_update(dt):
-                self._init_scene()
+                self.__init_scene()
         if self._run_enter:
             if self._actual_scene.on_enter_update(dt):
                 self._run_enter = False
 
-    def _render_transtion(self, display: pygame.Surface):
+    def __render_transition(self, display: pygame.Surface):
         """Update the current scene transition"""
-
         if self._run_exit:
             self._actual_scene.on_exit_render(display)
         if self._run_enter:
@@ -61,37 +68,35 @@ class SceneManager:
 
     def add_scene(self, name: str, scene: CallableScene):
         """Adds new scene to scenes"""
-
         self._scenes[name] = scene
 
     def remove_scene(self, name: str):
         """Removes scene from scenes"""
-
         if name in self._scenes:
             del self._scenes[name]
 
     def change_scene(self, name: str):
         """Method to change scenes if name is avalible in scenes"""
-
         if name in self._scenes and not (self._run_enter or self._run_exit):
-            self._next_scene = name
+            self._next_scene_name = name
             if not self._actual_scene:
-                self._init_scene()
+                self.__init_scene()
             else:
                 self._run_exit = True
 
-    def _init_scene(self):
-        self._actual_scene: Scene2D = self._scenes[self._next_scene].scene_class(
-            **self._scenes[self._next_scene].kwargs
+    def __init_scene(self):
+        """Instantiate the next scene"""
+        self._actual_scene: Scene2D = self._scenes[self._next_scene_name].scene_class(
+            **self._scenes[self._next_scene_name].kwargs
         )
         self._run_enter = True
         self._run_exit = False
+        self._previus_scene_name = self._actual_scene_name
         self._actual_scene.set_scene_manager(self)
-        self._actual_scene_name = self._next_scene
+        self._actual_scene_name = self._next_scene_name
 
     def scenes_names(self) -> set:
         "Get all scenes names"
-
         return set(self._scenes.keys())
 
     @property
