d4855deef7e78f1da1a769f4f0d241b8160c5bdc
KentAugust <kentaugustone@gmail.com>
2023-10-02 09:57:35 -0600

    Add test_init_game & test_start_fullscreen test

--- tests/game.py
+++ tests/game.py
@@ -1,7 +1,7 @@
 import unittest
 import pygame
 
-from src import WindowScreen, Game,EventHandler, SceneManager
+from src import WindowScreen, Game,EventHandler, SceneManager, Scene2D
 from src.config import WindowConfig, GameConfig
 from src.timers import Delta
 from src.inputs import Keyboard, Mouse
@@ -27,12 +27,19 @@ class TestGameType(unittest.TestCase):
             self.assertEqual(Delta, type(g.deltatimer))
             self.assertEqual(Vec2, type(g.display_offset))
 
+    def test_init_game(self):
+        with WindowContex(WindowScreen, wc) as w:
+            g = Game(gc, w)
+            g.init_game()
+            self.assertTrue(pygame.display.get_init())
+            self.assertEqual(Scene2D, type(g.scene_manager.actual_scene))
+
     def test_set_title(self):
         with WindowContex(WindowScreen, wc) as w:
             g = Game(gc, w)
-            w.init_screen()
+            g.init_game()
             title = pygame.display.get_caption()
-            self.assertEqual("pygame window", title[0])
+            self.assertEqual("TestWindow", title[0])
             g.set_title("New Title")
             title = pygame.display.get_caption()
             self.assertEqual("New Title", title[0])
@@ -40,7 +47,7 @@ class TestGameType(unittest.TestCase):
     def test_quit(self):
         with WindowContex(WindowScreen, wc) as w:
             g = Game(gc, w)
-            w.init_screen()
+            g.init_game()
             self.assertTrue(pygame.display.get_init())
             try:
                 g.quit()
@@ -50,7 +57,15 @@ class TestGameType(unittest.TestCase):
     def test_update_win_size(self):
         with WindowContex(WindowScreen, wc) as w:
             g = Game(gc, w)
-            w.init_screen()
+            g.init_game()
             self.assertEqual((720, 480), w.current_size)
             self.assertEqual((360, 240), g.update_win_size(2))
             self.assertEqual((360, 240), w.current_size)
+
+    def test_start_fullscreen(self):
+        with WindowContex(WindowScreen, wc) as w:
+            gcc = GameConfig("TestWindow", 60, True)
+            g = Game(gcc, w)
+            g.init_game()
+            self.assertTrue(w.is_fullscreen)
+            self.assertEqual(w.desktop_sizes[0], w.current_size)
