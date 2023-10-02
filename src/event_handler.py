3a2052dec190cf5f22325349b865f789bda9d037
KentAugust <kentaugustone@gmail.com>
2023-10-02 10:58:09 -0600

    Add poll & peek methods

--- src/event_handler.py
+++ src/event_handler.py
@@ -10,13 +10,38 @@ class EventHandler:
 
     def __init__(self, game: Game) -> None:
         self.game = game
+        self._last_event = pygame.Event(pygame.NOEVENT)
 
-    def loop(self):
+    def _handle_event(self, event: pygame.Event):
+        """Handle a single event"""
+        self.game.keyboard.handle_event(event)
+        self.game.mouse.handle_event(event)
+        self._last_event = event
+        match event.type:
+            case pygame.QUIT:
+                self.game.quit()
+
+    def loop(self) -> bool:
         """Event loop"""
-
         for event in pygame.event.get():
-            self.game.keyboard.handle_event(event)
-            self.game.mouse.handle_event(event)
-            match event.type:
-                case pygame.QUIT:
-                    self.game.quit()
+            self._handle_event(event)
+        return True
+
+    def poll(self) -> bool:
+        """Event poll"""
+        event = pygame.event.poll()
+        self._handle_event(event)
+        if event == pygame.NOEVENT:
+            return False
+        return True
+
+    def peek(self) -> bool:
+        """Event peek"""
+        if pygame.event.peek():
+            return self.poll()
+        return False
+
+    @property
+    def last_event(self):
+        """last Event type"""
+        return self._last_event
